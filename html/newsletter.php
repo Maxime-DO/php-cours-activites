9f2aba998ec3a4903bdbf9cf18a54ab3a2ff6582
Maxime_DO <maxigow@gmail.com>
2019-10-21 16:57:13 +0200

    bonus write & read a file

---
+++ html/newsletter.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Newsletter post</title>
+  </head>
+<body>
+  <h1>Newsletter</h1>
+  <form action="/newsletter.php" method="GET">
+    <label for="email">Abonne toi</label>
+    <input type="email" name="email" id="email" value="" placeholder="mon_mail@example.com" required>
+    <button>Envoyer</button>
+  </form>
+
+  <form action="/newsletter.php" method="GET">
+      <button name="files">Afficher les email</button>
+  </form>
+
+<?php
+
+  if (isset($_REQUEST['email'])) {
+    // echo ($_REQUEST['email']);
+    file_put_contents(__DIR__.'/listeEmail.txt', ($_REQUEST['email'] . "\n"), FILE_APPEND);
+  } elseif (isset($_REQUEST['files'])) {
+    $liste_de_mail = file_get_contents(__DIR__.'/listeEmail.txt');
+    echo $liste_de_mail . "<br/>";
+  }
+?>
+
+</body>
+</html>
