a0e8695bd4d9bc5f9c915c142f8bf82004f36ad6
Maxime_DO <maxigow@gmail.com>
2019-10-21 12:31:49 +0200

    résolution des exo if

--- conditions/if-compose-avance.php
+++ conditions/if-compose-avance.php
@@ -9,9 +9,9 @@ $vivant = true;
 $couleur = "Blanc";
 $yeux = "Vert";
 
-echo "1.Est ce que c'est mon chat ? ";
-echo "<br>";
 echo "Mon chat est vivant, de couleur blanc et a les yeux Vert";
+echo "<br>";
+echo "1.Est ce que c'est mon chat ? ";
 echo "<br>";
 if($animal == "Chat" && $vivant && $couleur = "Blanc" && $yeux = "Vert"){
   echo "Oui";
@@ -26,20 +26,20 @@ echo "<br><br>";
 /*###############################################*/
 
 // Écrivez le test qui vérifie la question suivante : (utilisez un seul if)
-echo "1.Est-ce que c'est un animal fait l'affaire ? ";
-echo "<br>";
 echo "Je veux un chien ou chat couleur marron avec des yeux verts ";
+echo "<br>";
+echo "1.Est-ce que cet un animal convient à mes critères ? ";
 echo "<br>";
 //n'hésitez pas à changer les valeurs pour tester si ça fonctionne
 $animal = "Chat";
 $vivant = true;
-$couleur = "Marron";
+$couleur = "Bleu";
 $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
-if( 0 ){
+if( ($animal == "Chat" || $animal == "Chien") && $vivant == true && $couleur == "Bleu" && $yeux == "Vert" ) {
   echo 'Oui';
-}else {
+} else {
   echo 'Non';
 }
 echo "<br><br>";
@@ -57,7 +57,7 @@ $couleur = "Blanc";
 $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
-if( 0 ){
+if( $animal == "Chat" && $vivant == true && ($couleur == "Marron" || $couleur == "Blanc") && $yeux == "Vert" ){
   echo 'Oui';
 }else {
   echo 'Non';
@@ -77,7 +77,7 @@ $couleur = "Noir";
 $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
-if( 0 ){
+if( ($animal == "Chien" && $yeux == "Bleu") || ($animal == "Chat" && $couleur == "orange" && $yeux == "Bleu") || ($animal == "Chien" && $couleur == "Marron" && $yeux == "Bleu")) {
   echo 'Oui';
 }else {
   echo 'Non';
